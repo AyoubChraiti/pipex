ca348c6d39107b2dc995b32c9909e03d2ba6eab8
Ayoub Chraiti <achraiti@e1r6p11.1337.ma>
2024-01-28 15:28:53 +0100

    added files

---
+++ pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achraiti <achraiti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/28 15:20:39 by achraiti          #+#    #+#             */
+/*   Updated: 2024/01/28 15:27:36 by achraiti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+
+#endif
