7be6efd7cba611e22976ce8df01771c4c4907975
Ayoub Chraiti <135065909+AyoubChraiti@users.noreply.github.com>
2024-02-16 13:07:25 +0100

    update

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: achraiti <achraiti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 13:55:38 by achraiti          #+#    #+#             */
-/*   Updated: 2024/01/25 17:42:49 by achraiti         ###   ########.fr       */
+/*   Updated: 2024/02/15 20:07:58 by achraiti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
 	if (!s1 && s2)
-		return ((char *)s2);
+		return (ft_strdup((char *)s2));
 	else if (s1 && !s2)
-		return ((char *)s1);
+		return (ft_strdup((char *)s1));
 	m = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!m)
 		return (0);
--- pipex.c
+++ pipex.c
@@ -6,23 +6,75 @@
 /*   By: achraiti <achraiti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 15:20:41 by achraiti          #+#    #+#             */
-/*   Updated: 2024/02/15 15:15:04 by achraiti         ###   ########.fr       */
+/*   Updated: 2024/02/15 22:37:09 by achraiti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char    *get_path(char **argv, char **env)
+char	*get_env(char **env)
 {
-    char    *path = &env[3][5];
-    char    **paths = ft_split(path, ':');
-    int i = 0;
-    while(paths[i] != NULL)
-    {
-        printf("%s\n", paths[i]);
-        i++;
-    }
-    return "hh";
+	int i = 0;
+	
+	while(env[i])
+	{
+		if(ft_strncmp(env[i], "PATH=", 5) == 0)
+			return (env[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+char *cmd_arguments(char **argv, int t)
+{
+	int count = 0;
+	char **sp = ft_split(argv[t], ' ');
+	char *res = NULL;
+	char *tmp = NULL;
+	int i = 1;
+	while(sp[count] != NULL)
+		count++;
+	if(count == 1)
+		return (NULL);
+	while(sp[i])
+	{
+		tmp = ft_strjoin(sp[i], " ");
+		res = ft_strjoin(res, tmp);
+		i++;
+	}
+	return (res);
+}
+
+char    *get_path_cmd(char **argv, char **env, int t)
+{
+	int flag = 1;
+	char *try;
+	char *path_var = get_env(env);
+	char **paths = ft_split(path_var, ':');
+	char **cmd1 = ft_split(argv[t], ' ');
+	int i = 0;
+	
+	while(paths[i] != NULL)
+	{
+		paths[i] = ft_strjoin(paths[i], "/");
+		i++;
+	}
+	i = 0;
+	while(paths[i] != NULL)
+	{
+		try = ft_strjoin(paths[i], cmd1[0]);
+		if(access(try, F_OK | X_OK) == 0)
+		{
+			flag = 1;
+			break ;
+		}
+		free(try);
+		i++;
+	}
+	printf("%d\n", flag);
+	if(flag)
+		return (try);
+	return (cmd1[0]);
 }
 
 void    pipex(int fd_a, int fd_b, char **argv, char **env)
@@ -38,7 +90,7 @@ void    pipex(int fd_a, int fd_b, char **argv, char **env)
         close(fd[0]);
         dup2(fd_a, 0);
         dup2(fd[1], 1);
-		char *const x[] = {"/bin/ls", "-l", NULL};
+		char *const x[] = {"/bin/ls", "-l", "-a", NULL};
 		execve("/bin/ls", x, env);
         perror("Error");
         exit(1);
@@ -49,7 +101,7 @@ void    pipex(int fd_a, int fd_b, char **argv, char **env)
         close(fd[1]);
         dup2(fd_b, 1);
         dup2(fd[0], 0);
-        char *const c[] = {"/usr/bin/grep", "p", NULL};
+        char *const c[] = {"/usr/bin/grep", "a", NULL};
         execve("/usr/bin/grep", c, NULL);
         perror("Error");
         exit(1);
@@ -58,14 +110,17 @@ void    pipex(int fd_a, int fd_b, char **argv, char **env)
 
 int main(int argc, char **argv, char **env)
 {
-	// int		fd_a;
-	// int		fd_b;
+	int		fd_a;
+	int		fd_b;
 
-	// fd_a = open(argv[1], O_CREAT | O_RDWR);
-	// fd_b = open(argv[4], O_CREAT | O_RDWR);
-    // if(fd_a == -1 || fd_b == -1)
-    //     perror("Files Error");
-	// pipex(fd_a, fd_b, argv, env);
-    get_path(argv, env);
+	fd_a = open(argv[1], O_CREAT | O_RDWR);
+	fd_b = open(argv[4], O_CREAT | O_RDWR);
+    if(fd_a == -1 || fd_b == -1)
+        perror("Files Error");
+	pipex(fd_a, fd_b, argv, env);
+	// char *res = get_path_cmd(argv, env, 2);
+	// printf("%s\n", res);
+	// char *x = cmd_arguments(argv, 2);
+	// printf("%s\n", x);
     return 0;
 }
